30f0213aacd49f59ef7e901f209113348d2e9d22
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-11-03 01:34:26 +0300

    updated department dao

---
+++ src/main/java/dao/DepartmentDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import models.Department;
+import models.DepartmentNews;
+import models.User;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+
+    void add(Department department);
+
+    List<Department> getAllDepartments();
+
+    Department getDepartmentById(int id);
+
+    List<DepartmentNews> getDepartmentNewsById(int id);
+
+    List<User> getDepartmentUsersById(int id);
+}
