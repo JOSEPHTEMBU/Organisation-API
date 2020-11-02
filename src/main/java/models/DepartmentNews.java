080c0cdaabfba3ce9066f4749249c2a430554a73
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-11-03 01:27:37 +0300

    updated department news

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,4 +1,20 @@
 package models;
 
-public class DepartmentNews {
+public class DepartmentNews extends GeneralNews {
+
+    private int departmentId;
+
+
+    public DepartmentNews(String title, String content, int departmentId) {
+        super(title, content);
+        this.departmentId = departmentId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
 }
